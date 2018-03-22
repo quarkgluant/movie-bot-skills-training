8001c27fc27996e040805b24c043bdfd28f20bfa
Patrice Cadiot <pathibul.r@gmail.com>
2018-03-22 19:26:23 +0100

    modifie le config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   // You can add your own open weather map token here (https://home.openweathermap.org/api_keys)
   OPENWEATHER_TOKEN: process.env.OPENWEATHER_TOKEN || '',
   // You can add your own tmdb token here (https://www.themoviedb.org/)
-  MOVIEDB_TOKEN: process.env.MOVIEDB_TOKEN || '',
+  MOVIEDB_TOKEN: process.env.MOVIEDB_TOKEN || 'bf8c9028a2e3a6fc95d32a840e42ad6a',
   PORT: process.env.PORT || 5000,
 };
